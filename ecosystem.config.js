f4c8a2e17980afd1071d02e442feb9d6b1007132
dimensionaltechs <61759973+dimensionaltechs@users.noreply.github.com>
2020-03-28 17:44:24 +0200

    update ecosystem for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+     name: 'Resource Allocation Mobile',
+     script: 'karma.conf.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'one two',
